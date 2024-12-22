b798da9bca32012ddb8ecdcf4ff116a62b6890d9
Rince Mathew <rincemathewmanattu@gmail.com>
2024-12-22 22:46:08 +0530

    firestore configer

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,32 @@
-"use clint"
+"use client"
+import addData from "../firebase/firestore/addData";
 import React from 'react'
 
 function Main() {
+  const handleForm = async () => {
+    const data = {
+      name: "John Snow",
+      house: "Stark",
+    };
+    const { result, error } = await addData("users", "user-id", data);
+
+    if (error) {
+      return console.log("Error adding data:", error);
+    }
+
+    console.log("Data added successfully:", result);
+  };
+
   return (
-    <div>main</div>
-  )
+    <>
+      <div>main dev</div>
+      <button onClick={handleForm}>Add Data</button>
+    </>
+  );
+
+  // return (
+  //   <div>main</div>
+  // )
 }
 
 export default Main
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Main from "./components/main";
+import Main from "./components/Main";
 
 export default function Home() {
   return (
